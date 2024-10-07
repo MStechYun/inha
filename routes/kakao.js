9f2c5c21a5283fdddb5bb391cba0f76e5a54d3a5
Hyunseung Yun <108418549+MStechYun@users.noreply.github.com>
2024-10-07 14:33:32 +0900

    Delete routes directory

--- routes/kakao.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/book', function(req, res, next) {
-  res.render('index', { title: '도서검색', pageName:'kakao/book.ejs' });
-});
-
-
-module.exports = router;
