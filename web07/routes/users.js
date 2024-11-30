6f435bc24480cdea932f4719f010a8d925ee5ad6
MStechYun <hyunseung.yoon7@gmail.com>
2024-12-01 02:24:54 +0900

    Initial commit

---
+++ web07/routes/users.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/login', function(req, res){
+  res.render('index', {title:'로그인', pageName:'users/login.ejs'})
+});
+
+//회원가입
+router.get('/join', function(req, res){
+  res.render('index', {title:'회원가입', pageName:'users/join.ejs'})
+});
+
+//장바구니
+router.get('/cart', function(req, res, next){
+  res.render('index', {title:'장바구니', pageName:'users/cart'})
+});
+
+module.exports = router;
---
+++ web11/routes/users.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/login', function(req, res){
+  res.render('index', {title:'로그인', pageName:'users/login.ejs'})
+});
+
+//회원가입
+router.get('/join', function(req, res){
+  res.render('index', {title:'회원가입', pageName:'users/join.ejs'})
+});
+
+//장바구니
+router.get('/cart', function(req, res, next){
+  res.render('index', {title:'장바구니', pageName:'users/cart.ejs'})
+});
+
+//마이패이지
+router.get('/mypage', function(req, res, next){
+  res.render('index', {title:'마이페이지', pageName:'users/mypage.ejs'})
+});
+
+module.exports = router;
